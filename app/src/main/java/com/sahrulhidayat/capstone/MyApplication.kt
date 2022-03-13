c69232de82cbc7de8231305c429540ba23140d75
sahrulhidayat <83418069+sahrulhidayat@users.noreply.github.com>
2022-03-14 00:08:31 +0700

    add dependency injection CoreModule

---
+++ app/src/main/java/com/sahrulhidayat/capstone/MyApplication.kt
@@ -0,0 +1,31 @@
+package com.sahrulhidayat.capstone
+
+import android.app.Application
+import com.sahrulhidayat.capstone.di.useCaseModule
+import com.sahrulhidayat.capstone.di.viewModelModule
+import com.sahrulhidayat.core.di.databaseModule
+import com.sahrulhidayat.core.di.networkModule
+import com.sahrulhidayat.core.di.repositoryModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.NONE)
+            androidContext(this@MyApplication)
+            modules(
+                listOf(
+                    databaseModule,
+                    networkModule,
+                    repositoryModule,
+                    useCaseModule,
+                    viewModelModule
+                )
+            )
+        }
+    }
+}
---
+++ core/src/main/java/com/sahrulhidayat/core/di/CoreModule.kt
@@ -0,0 +1,57 @@
+package com.sahrulhidayat.core.di
+
+import androidx.room.Room
+import com.sahrulhidayat.core.BuildConfig
+import com.sahrulhidayat.core.data.source.GameRepository
+import com.sahrulhidayat.core.data.source.local.LocalDataSource
+import com.sahrulhidayat.core.data.source.local.room.GameDatabase
+import com.sahrulhidayat.core.data.source.remote.RemoteDataSource
+import com.sahrulhidayat.core.data.source.remote.network.ApiService
+import com.sahrulhidayat.core.domain.repository.IGameRepository
+import com.sahrulhidayat.core.utils.AppExecutors
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val databaseModule = module {
+    factory { get<GameDatabase>().gameDao() }
+
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            GameDatabase::class.java, "game_entities"
+        ).fallbackToDestructiveMigration().build()
+    }
+}
+
+val networkModule = module {
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+
+    single {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
+
+val repositoryModule = module {
+    single { LocalDataSource(get()) }
+    single { RemoteDataSource(get()) }
+    single { AppExecutors() }
+    single<IGameRepository> {
+        GameRepository(get(), get(), get())
+    }
+}
