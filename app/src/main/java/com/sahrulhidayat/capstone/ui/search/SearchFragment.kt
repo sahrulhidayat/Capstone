fb90a80672378988ef54715189bcee7d647b96ca
sahrulhidayat <83418069+sahrulhidayat@users.noreply.github.com>
2022-03-17 14:17:28 +0700

    update SearchFragment

--- app/src/main/java/com/sahrulhidayat/capstone/ui/search/SearchFragment.kt
+++ app/src/main/java/com/sahrulhidayat/capstone/ui/search/SearchFragment.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sahrulhidayat.capstone.databinding.FragmentSearchBinding
+import com.sahrulhidayat.core.ui.GameAdapter
+import com.sahrulhidayat.core.utils.gone
+import com.sahrulhidayat.core.utils.visible
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SearchFragment : Fragment() {
 
     private var _fragmentSearchBinding: FragmentSearchBinding? = null
     private val binding get() = _fragmentSearchBinding
+
+    private val viewModel by viewModel<SearchViewModel>()
+    private val gameAdapter: GameAdapter by lazy { GameAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,8 +32,37 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding?.svSearchGame?.requestFocus()
+        binding?.rvSearch?.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = gameAdapter
+        }
 
+        binding?.svSearchGame?.apply {
+            requestFocus()
+            setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    getSearchedGames(newText.orEmpty())
+                    return true
+                }
+
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    getSearchedGames(query.orEmpty())
+                    return true
+                }
+            })
+        }
+    }
+
+    private fun getSearchedGames(name: String) {
+        viewModel.getSearchedGames(name).observe(requireActivity()) { games ->
+            gameAdapter.setData(games)
+        }
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) binding?.progressBar?.visible()
+        else binding?.progressBar?.gone()
     }
 
     override fun onDestroyView() {
