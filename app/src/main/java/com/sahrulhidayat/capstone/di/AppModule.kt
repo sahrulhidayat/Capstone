9a39b53f12ee40ebcfc75d698db7532e433f0c2b
sahrulhidayat <83418069+sahrulhidayat@users.noreply.github.com>
2022-04-15 13:58:53 +0700

    add MainViewModel UnitTest
    [ci skip]

--- app/src/main/java/com/sahrulhidayat/capstone/di/AppModule.kt
+++ app/src/main/java/com/sahrulhidayat/capstone/di/AppModule.kt
@@ -1,9 +1,10 @@
 package com.sahrulhidayat.capstone.di
 
 import com.sahrulhidayat.capstone.ui.MainViewModel
+import com.sahrulhidayat.core.utils.DispatcherProvider
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    viewModel { MainViewModel(get()) }
+    viewModel { MainViewModel(DispatcherProvider.main(), get()) }
 }
--- app/src/main/java/com/sahrulhidayat/capstone/ui/MainViewModel.kt
+++ app/src/main/java/com/sahrulhidayat/capstone/ui/MainViewModel.kt
@@ -4,9 +4,18 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.sahrulhidayat.core.domain.usecase.PreferenceUseCase
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 
-class MainViewModel(private val preferenceUseCase: PreferenceUseCase) : ViewModel() {
-    fun getThemeSettings(): LiveData<Boolean> {
-        return preferenceUseCase.getThemeSettings().asLiveData()
+class MainViewModel(
+    private val mainDispatcher: CoroutineDispatcher,
+    private val preferenceUseCase: PreferenceUseCase
+) : ViewModel() {
+
+    fun getThemeSettings(): LiveData<Boolean> = getThemeSettingsFlow().asLiveData()
+
+    fun getThemeSettingsFlow(): Flow<Boolean> {
+        return preferenceUseCase.getThemeSettings().flowOn(mainDispatcher)
     }
 }
